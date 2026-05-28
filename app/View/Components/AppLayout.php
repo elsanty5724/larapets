5f76f80d21ff90407f0a4e40c2c90ec80252bbba
santiago <santiagoarevalom10@gmail.com>
2026-05-27 20:16:01 -0500

    message

--- app/View/Components/AppLayout.php
+++ app/View/Components/AppLayout.php
@@ -9,6 +9,7 @@ class AppLayout extends Component
 {
     /**
      * Get the view / contents that represents the component.
+     * @return View
      */
     public function render(): View
     {
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'ca468a39784a6d25a0f38283b9271a612a9793ad',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -443,9 +443,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'ca468a39784a6d25a0f38283b9271a612a9793ad',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
