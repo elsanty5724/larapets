0f5a9facd02445d8d2fb1c8b3baa6428b9b2b7a2
santiago <santiagoarevalom10@gmail.com>
2026-06-30 20:12:09 -0500

    modify

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
 @endauth
 
 @php
-    $background = request()->is('login') ? asset('images/welcome.jpg') : asset('images/dashboard/illustration-emblem-for-pet-shop-veterinary-clinic-animal-shelter-vector.jpg');
+    $background = request()->is('login') ? asset('images/welcome.jpg') : asset('images/dashboard/illustration-emblem-for-pet-shop-veterinary-clinic-animal-shelter-vector.avif');
 @endphp
 
 <body class="bg-[linear-gradient(to_top,{{$colors}}),url('{{ $background }}')] bg-center bg-no-repeat bg-cover bg-fixed bg-black text-white pt-14 min-h-dvh flex flex-col gap-2 justify-center items-center">
